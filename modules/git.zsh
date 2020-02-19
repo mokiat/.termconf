2032d8289ee3ff1e39500f066b0de059fcd9f464
Momchil Atanasov <momchil.dev@gmail.com>
2020-02-19 22:35:59 +0200

    add git tree alias

--- modules/git.zsh
+++ modules/git.zsh
@@ -1,3 +1,4 @@
 git config --global 'alias.s' 'status'
 git config --global 'alias.br' 'branch'
 git config --global 'alias.last' 'log -1'
+git config --global 'alias.tree' 'log --graph --pretty=oneline --abbrev-commit'
