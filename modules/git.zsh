d2296423f64c946c6f93e79d1ae57e5614700b18
Momchil Atanasov <momchil.dev@gmail.com>
2020-10-09 18:14:28 +0300

    improve go & git modules

--- modules/git.zsh
+++ modules/git.zsh
@@ -1,4 +1,5 @@
 git config --global 'alias.s' 'status'
 git config --global 'alias.br' 'branch'
+git config --global 'alias.pl' 'pull --ff-only'
 git config --global 'alias.last' 'log -1'
 git config --global 'alias.tree' 'log --graph --pretty=oneline --abbrev-commit'
--- modules/go.zsh
+++ modules/go.zsh
@@ -1,4 +1,2 @@
-export GOOS=darwin
-export GOARCH=amd64
 export GOPATH="$HOME/.go"
 export PATH="$PATH:$GOPATH/bin"
