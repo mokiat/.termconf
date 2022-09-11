02b2d63288eabbfa55f819302ee40409e6b28990
Momchil Atanasov <momchil.dev@gmail.com>
2022-09-11 16:55:39 +0300

    docker: add enhancements

--- modules/docker.zsh
+++ modules/docker.zsh
@@ -1,2 +1,12 @@
+export DOCKER_CLIENT_TIMEOUT=240 # default 60
+export DOCKER_BUILDKIT=1
+export COMPOSE_HTTP_TIMEOUT=240 # default 60
+export COMPOSE_PARALLEL_LIMIT=15 # default 64
+export COMPOSE_DOCKER_CLI_BUILD=1
+
 alias d='docker'
 alias dc='docker-compose'
+
+function dclearimg() {
+  docker rmi $(docker images | awk '{ print $3 " -> " $1 }' | grep '<none>' | awk '{ print $1 }')
+}
